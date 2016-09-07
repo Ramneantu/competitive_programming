65e02d8798d87dcbc693565549e6b291045486c5
Cristian Plop <ga57far@mytum.de>
2016-09-07 21:45:31 +0200

    add solution to TUMJUDGE Collection 2 Problem C, God this was a tricky one to implement. Trivial, but tricky

--- TUMJUDGE/PROBLEM_COLLECTION_VOLUME_2/C.cpp
+++ TUMJUDGE/PROBLEM_COLLECTION_VOLUME_2/C.cpp
@@ -1,58 +1,81 @@
 #include <iostream>
-#include <cstring>
 #include <algorithm>
+#include <numeric>
+#include <set>
+#include <map>
+#include <queue>
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <cctype>
+#include <cassert>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <utility>
 
+#define rep(i,a,b) for(int i=int(a); i<=int(b); ++i)
+#define rev(i,b,a) for(int i=int(b); i>=int(a); --i)
+#define mp(x,y) make_pair((x),(y))
+#define pb(x) push_back(x)
+#define all(c) c.begin(), c.end()
+#define tr(container, it) for(auto it=(container).begin(); it != (container).end(); ++it)
+#define sqr(x) ((x)*(x))
 using namespace std;
 
-int steps(string line){
-  int left, right, cur, moves;
-  moves = 0;
-  cur = 0;
-  int i;
-  for(i=0; i<line.length() && line[i] == 'A'; ++i);
-  left = i;
-  //cout << "left: " << left << "\n";
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
 
-  for(i=line.length()-1; i>= 0 && line[i] == 'A'; --i);
-  right = i;
-  //cout << "right: " << right << "\n";
-
-  while(left <= right){
-    //cout << "line: " << line << "\n";
-    int dist_left, dist_right;
-    if(left < cur)
-      dist_left = line.length() - (cur - left);
-    else
-      dist_left = left - cur;
-    if(right < cur)
-      dist_right = cur - right;
-    else
-      dist_right = line.length() - (right - cur);
-
-    //cout << "dist_to_left: " << dist_left << " dist_to_right: " << dist_right << "\n";
-
-    if(dist_left < dist_right){
-      cur = left;
-      moves += dist_left;
-      int letter_dist = line[left]-'A';
-      line[left] = 'A';
-      moves += min(letter_dist, 26-letter_dist);
-      for(i=cur; i<line.length() && line[i] == 'A'; ++i);
-      left = i;
-    }
-    else{
-      cur = right;
-      moves += dist_right;
-      int letter_dist = line[right]-'A';
-      line[right] = 'A';
-      moves += min(letter_dist, 26-letter_dist);
-      for(i=cur; i>=0 && line[i] == 'A'; --i);
-      right = i;
-    }
-  }
-  return moves;
+int mod_clip(int v, int mod){
+  v = v % mod;
+  if(v<0)
+    return mod + v;
+  return v;
 }
 
+
+int find_left(string s, int pos){
+  if(s[pos] != 'A')
+    return pos;
+  int i = mod_clip(pos-1, s.size());
+  while(i != pos){
+    if(s[i] != 'A')
+      return i;
+    i = mod_clip(i-1, s.size());
+  }
+  return i;
+}
+
+int find_right(string s, int pos){
+  if(s[pos] != 'A')
+    return pos;
+  int i = mod_clip(pos+1, s.size());
+  while(i != pos){
+    if(s[i] != 'A')
+      return i;
+    i = mod_clip(i+1, s.size());
+  }
+  return i;
+}
+
+int dist_left(int pos, int l, int n){
+  if(pos >= l)
+    return pos-l;
+  else
+    return pos+n-l;
+}
+
+int dist_right(int pos, int r, int n){
+  if(pos <= r)
+    return r-pos;
+  else
+    return r+n-pos;
+}
 
 int main(){
 
@@ -62,14 +85,64 @@ int main(){
   for(int t=1; t<=T; ++t){
     string line;
     cin >> line;
+    int n = line.size();
 
-    int s1 = steps(line);
-    reverse(line.begin(), line.end());
-    int s2 = steps(line);
+    int total = 0;
+    for(int i=0; i<line.size(); ++i)
+      if(line[i] != 'A'){
+        int letter_dist = line[i]-'A';
+        total += min(letter_dist, 26-letter_dist);
+      }
 
-    cout << s1 << " " << s2 << "\n";
+    int best = 100000000;
+    int moves;
+    for(int i=0; i<line.size(); ++i){
+      if(line[i] != 'A'){
+        string s = line;
+        int pos = i;
+        if(i < n-i){
+          moves = i;
+          rep(k,0,i)
+            s[k] = 'A';
+        }
+        else{
+          moves = n-i;
+          rev(k,s.size()-1,i)
+            s[k] = 'A';
+        }
 
-    cout << "Case #" << t << ": " << min(s1, s2+1) << "\n";
+        int l = find_left(s, pos);
+        int r = find_right(s, pos);
+
+        int ct = 0;
+        while(l != r){
+          int go_left = dist_left(pos, l, n);
+          int go_right = dist_right(pos, r, n);
+
+          if(go_left == 0 && go_right == 0)
+            break;
+
+          if(go_left < go_right){
+            moves += go_left;
+            pos = l;
+            s[pos] = 'A';
+            l = find_left(s, pos);
+          }
+          else{
+            moves += go_right;
+            pos = r;
+            s[pos] = 'A';
+            r = find_right(s, pos);
+          }
+
+        }
+        moves += min(dist_left(pos, l, n), dist_right(pos, r, n));
+        best = min(best, moves);
+      }
+    }
+    if(best >= 100000000)
+      best = 0;
+    printf("Case #%d: %d\n", t, best + total);
   }
 
   return 0;
