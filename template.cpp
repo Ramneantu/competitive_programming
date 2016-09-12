60243b38fb2ec9c3aaad5590c2391294c118df20
Cristian Plop <ga57far@mytum.de>
2016-09-12 17:04:04 +0200

    add macros to the competitive programming template.

--- template.cpp
+++ template.cpp
@@ -1,27 +1,39 @@
-#include <iostream>
-#include <algorithm>
-#include <numeric>
-#include <set>
 #include <map>
-#include <queue>
-#include <iostream>
-#include <sstream>
-#include <cstdio>
+#include <set>
+#include <list>
 #include <cmath>
 #include <ctime>
-#include <cstring>
-#include <cctype>
-#include <cassert>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
 #include <limits>
-#include <functional>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <bitset>
+#include <utility>
 
 #define rep(i,a,b) for(int i=int(a); i<=int(b); ++i)
-#define rev(i,b,a) for(int i=int(b); i>=int(a); --i)
+#define rev(i,a,b) for(int i=int(a); i>=int(b); --i)
+#define rec(i,a,b,c) for(int i=int(a); i<=int(b); i+=int(c))
+#define recv(i,a,b,c) for(int i=int(a); i>=int(b); i-=int(c))
 #define mp(x,y) make_pair((x),(y))
 #define pb(x) push_back(x)
 #define all(c) c.begin(), c.end()
 #define tr(container, it) for(auto it=(container).begin(); it != (container).end(); ++it)
 #define sqr(x) ((x)*(x))
+#define sz(a) int((a).size())
+#define mod(a,n) ((a) < 0 ? ((n)+(a)) : ((a)%(n)))
 using namespace std;
 
 typedef vector<int> vi;
