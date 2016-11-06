c00b4d1dbff5481397f5c8dacfb9c089a8c95341
Cris Pi <ga57far@mytum.de>
2016-11-06 02:55:56 +0100

    Update helper.sh

--- helper.sh
+++ helper.sh
@@ -2,5 +2,7 @@ judge(){
     problem="$1";
     g++ $problem.cpp;
     [ $? -ne 0  ] && echo "Could not compile $problem." && return;
-    for i in $(ls | grep $problem.*.in); do ./a.out < $i > mine && diff ${i%.*}.ok mine; [ $? -ne 0 ] && echo "TESTCASE $i FAILED!" ; done
+    for i in $(ls | grep $problem.*.in); do ./a.out < $i > mine; [ ! -e ${i%.*}.ok ] && echo "OK file for TESTCASE $i is missing. Skipping TESTCASE $i" && continue; diff ${i%.*}.ok mine; [ $? -ne 0 ] && echo "TESTCASE $i FAILED!" ; done
+
+    echo "PASSED!";
 }
