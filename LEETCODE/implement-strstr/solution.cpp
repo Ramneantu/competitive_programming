3e7de9b6582ff60420cb75cc388e5b7e742df254
Cristian Plop <cristian.plop@tum.de>
2017-01-17 16:26:21 +0100

    close #3.

--- LEETCODE/implement-strstr/solution.cpp
+++ LEETCODE/implement-strstr/solution.cpp
@@ -75,26 +75,20 @@ typedef pair<int, int> ii;
 typedef vector<ii> vii;
 typedef long long ll;
 
-ass Solution {
+class Solution {
 public:
     int strStr(string haystack, string needle) {
-        if(needle.size() == 0 && haystack.size() == 0) return 0;
-        if(haystack.size() > 0 && needle.size() == 0) return 0;
-        if(haystack.size() < needle.size()) return -1;
-        if(needle.size() == 0 || haystack.size() == 0) return -1;
-        for(int i=0;i<=haystack.size()-needle.size();++i){
-            bool found = true;
-            for(int j=0; j<needle.size();++j)
-                if(haystack[i+j] != needle[j]){
-                    found = false;
-                    break;
-                }
-            if(found)
-                return i;
+        int i,j;
+        for(i=0; i<=int(haystack.size()-needle.size()); ++i){
+            for(j=0; j<needle.size(); ++j)
+                if(haystack[i+j] != needle[j])  break;
+            if(j >= needle.size())  return i;
         }
         return -1;
     }
 };
+
+
 int main(){
 
 
