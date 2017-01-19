2c8dca5a78b23e30ec078611bc5483f7a58e1f0b
Cristian Plop <cristian.plop@tum.de>
2017-01-19 23:53:19 +0100

    add week 11.

--- TUMJUDGE/CONPRA1617/WEEK11/A/A.cpp
+++ TUMJUDGE/CONPRA1617/WEEK11/A/A.cpp
@@ -52,15 +52,69 @@ typedef long long ll;
 
 
 int main(){
-
   int t;
   cin >> t;
   rep(_t,1,t){
+    int m, n; cin >> m >> n;
+    vector<vector<int> > Is;
+    rep(i,0,n-1){
+        int p,l,s; cin >> p >> l >> s;
+        vector<int> _tmp(3,0);
+        _tmp[0] = i;
+        _tmp[1] = l;
+        _tmp[2] = s;
+        rep(j,0,p-1)
+          Is.pb(_tmp);
+    }
+    vector<vector<int> > dp(sz(Is), vector<int>(m+1,0));
 
-    // your magic
+    DBG(
+      rep(i,0,sz(Is)-1)
+        cout << Is[i][1] << " " << Is[i][2] << endl;
+    )
 
+    rep(j,Is[0][1],m)
+      dp[0][j] = Is[0][2];
+
+    rep(i,1,sz(Is)-1){
+      rep(j,0,m){
+          if(Is[i][1] > j)
+            dp[i][j] = dp[i-1][j];
+          else
+              dp[i][j] = max(dp[i-1][j], Is[i][2] + dp[i-1][j-Is[i][1]]);
+      }
+    }
     cout << "Case #" << _t << ": ";
-    // your output
+
+    DBG(
+      cout <<"\n";
+      rep(i,0,sz(Is)-1){
+        rep(j,0,m)
+          cout << dp[i][j] << " ";
+        cout << "\n";
+      }
+    )
+
+    // find max
+    ll maxim = 0;
+    int pos = -1;
+    rep(j,0,m)
+      if(maxim < dp[Is.size()-1][j]){
+        maxim = dp[Is.size()-1][j];
+        pos = j;
+      }
+    DBG(cout << "maxim: "  << maxim << endl;)
+    int i = sz(Is)-1;
+    int j = pos;
+    while(i > 0 && j >= 0){
+      if(dp[i-1][j] < dp[i][j]){
+        cout << Is[i][0]+1 << " ";
+        j -= Is[i][1];
+      }
+      i--;
+    }
+    if(dp[i][j] >= Is[i][1])
+      cout << Is[i][0]+1;
     cout << "\n";
   }
 
