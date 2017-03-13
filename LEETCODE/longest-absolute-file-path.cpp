11642926a3799d7a31ef19485669c2c457486596
Cristian Plop <cristian.plop@tum.de>
2017-03-13 21:02:17 +0100

    clean last solution. shorten solution template.

--- LEETCODE/longest-absolute-file-path.cpp
+++ LEETCODE/longest-absolute-file-path.cpp
@@ -21,32 +21,25 @@ struct TreeNode {
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-
-
-typedef vector<int> vi;
-typedef pair<int, int> ii;
-typedef vector<ii> vii;
-typedef long long ll;
-
 class Solution {
 public:
     int lengthLongestPath(string input) {
         int maxlength = 0;
         istringstream sin(input);
-        map<int,int> H;
+        map<int,int> H{{-1,-1}};
         for(string line; getline(sin, line);){
             int depth = line.find_last_of('\t') + 1;
             int curlen = int(line.length()) - depth + 1;
-            curlen -= depth == 0 ? 1 : 0;
             if(line.find('.') != string::npos){ // we have a filename.
-                maxlength = max(maxlength, depth > 0 ? H[depth-1] + curlen : curlen);
+                maxlength = max(maxlength, H[depth-1] + curlen);
             }
             else
-                H[depth] = depth > 0 ? H[depth-1] + curlen : curlen;
+                H[depth] = H[depth-1] + curlen;
         }
         return maxlength;
     }
 };
+
 
 int main(){
 
