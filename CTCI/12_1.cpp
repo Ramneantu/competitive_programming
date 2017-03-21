b26cf4f71538a9c0b1aa3b57f48e0cfd70ab017a
Cristian Plop <cristian.plop@tum.de>
2017-03-21 18:14:38 +0100

    clean last solution.

--- CTCI/12_1.cpp
+++ CTCI/12_1.cpp
@@ -1,8 +1,15 @@
-#include <bits/stdc++.h>
+//====================================
+// Name: 12_1 CTCI Volume 6
+// Date: 21/03/17
+// Author: Cristian Plop
+// Copyright: Please refer to author's name in your contribution.
+// Description: Read last k lines of a file.
+//====================================
+
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-
   // read last k lines of a file
   ifstream f("file.in");
   ifstream g("file.in");
@@ -21,6 +28,4 @@ int main(){
   // g now has to go the last k lines.
   for(; getline(g,line); )
     cout << line << endl;
-
-  return 0;
 }
