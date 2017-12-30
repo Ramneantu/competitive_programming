e392f321dbbe5f2b8baf71d4c75e5e2264a2badb
Cristian Plop <cristian.plop@tum.de>
2017-12-31 03:13:01 +0800

    clean.

--- HACKERRANK/ML/digital_image_analysis/count_the_circles/count_the_circles.cpp
+++ HACKERRANK/ML/digital_image_analysis/count_the_circles/count_the_circles.cpp
@@ -21,6 +21,14 @@ using namespace std;
 /**
  * Kernels
  */
+vector<vector<double> > laplace3x3
+                            {
+                                {-1, 0, 1},
+                                {-2, 0, 2},
+                                {-1, 0, 1}
+                            };
+
+
 vector<vector<double> > sobel_x =
                             {
                                 {-1, 0, 1},
@@ -33,6 +41,11 @@ vector<vector<double> > sobel_y = {
                                 { 0,  0,  0},
                                 { 1,  2,  1}
                             };
+
+
+inline bool in_bounds(int i, int j, int n, int m) {
+    return i >= 0 && i<n && j >= 0 && j < m;
+}
 
 
 /**
@@ -150,6 +163,10 @@ public:
 
 
 
+
+/**
+ * Grayscale image class
+ */
 class IGRAY {
 private:
     size_t rows_;
@@ -251,6 +268,7 @@ public:
     }
 
 
+
     IGRAY erode(const vector<vector<double> >& pattern) {
 
       int pn = pattern.size();
@@ -335,116 +353,6 @@ IGRAY rgb2gray (IRGB& img) {
 
 
 vector<pair<int,int> > neighbors {{0,-1}, {-1,-1}, {-1,0}, {-1,1}, {0,1}, {1,1}, {1,0}, {1,-1}};
-//vector<pair<int,int> > neighbors {{0,-1}, {-1,0}, {0,1}, {1,0}};
-
-
-inline bool in_bounds(int i, int j, int n, int m) {
-    return i >= 0 && i<n && j >= 0 && j < m;
-}
-
-void dfs(vector<vector<int> >& mat, int i, int j, int& label, IGRAY& gray) {
-
-    stack<pair<int,int> > S;
-    S.push({i,j});
-
-    while (!S.empty()) {
-
-        auto e = S.top();
-        S.pop();
-        int ii = e.first;
-        int jj = e.second;
-
-        if (mat[ii][jj] == -1 && gray.at(ii,jj) >= 255) {
-            mat[ii][jj] = label;
-
-            gray.at(ii,jj,0);
-        }
-        else 
-            continue;
-
-        for (auto n : neighbors) {
-            if (in_bounds(ii+n.first, jj+n.second, mat.size(), mat[0].size())) {
-                S.push({ii+n.first, jj+n.second});
-            }
-        }
-    }
-}
-
-
-
-bool is_circle (int i, int j, vector<vector<int> >& viz, vector<vector<int> >& seg) {
-    
-    
-
-    stack<pair<int,int> > S;
-    S.push({i,j});
-    viz[i][j] = 1;
-
-    int min_c = INT_MAX;
-    int max_c = INT_MIN;
-    int min_r = INT_MAX;
-    int max_r = INT_MIN;
-    int area = 0;
-
-    while (!S.empty ()) {
-
-        auto e = S.top();
-        S.pop();
-
-        int ii = e.first;
-        int jj = e.second;
-
-
-        for (auto n : neighbors) {
-            int ni = ii+n.first;
-            int nj = jj+n.second;
-            if (!in_bounds(ni, nj, viz.size(), viz[0].size())) {
-    //            return false;
-                continue;
-            }
-
-            if (viz[ni][nj] == 0 && seg[ni][nj] != -1) {
-                S.push({ni,nj});
-                viz[ni][nj] = 1;
-
-                max_c = max(max_c, jj);
-                min_c = min(min_c, jj);
-                max_r = max(max_r, ii);
-                min_r = min(min_r, ii);
-
-                area++;
-
-            }
-        }
-    }
-
-    double r = min(max_c - min_c, max_r - min_r) / 2;
-    double est_area = M_PI*r*r;
-    double thresh = 2*M_PI*r * 4;
-    /*
-    cout << "est area: " << est_area << endl;
-    cout << "cur area: " << area << endl;
-    cout << "radius: " << r << endl;
-    cout << "thresh: " << thresh << endl;
-    cout << endl;
-    */
-
-    if (fabs(area-est_area) > thresh) {
-        return false;
-    }
-    return true;
-}
-
-
-vector<vector<double> > laplace3x3
-                            {
-                                {-1, 0, 1},
-                                {-2, 0, 2},
-                                {-1, 0, 1}
-                            };
-
-int ksize = 3;
-double sigma = 1;
 
 
 vector<vector<double> > get_cross (int h2, int w2) {
@@ -489,53 +397,6 @@ void print_bin_mat(vector<vector<double> >& mat) {
         }
         cout << endl;
     }
-}
-
-void test_cross (int k) {
-
-    for (int i=1; i<=k; ++i) {
-        cout << "for k: " << k << endl;
-
-        auto cr = get_cross(i,i);
-
-        print_bin_mat(cr);
-
-        cout << endl;
-    }
-}
-
-
-int get_n_circles (IGRAY img) {
-
-    double bg_val = img.at(10,10);
-    //cout << "Intensity = " << bg_val << endl << endl;
-
-    vector<vector<int> > seg(img.rows(), vector<int>(img.cols(), -1));
-    int label = 1;
-    int cols = img.cols();
-    int rows = img.rows();
-    for (int r = 0; r<rows; ++r) {
-        for (int c = 0; c < cols; ++c) {
-            if (seg[r][c] == -1 && img.at(r,c) >= 255) { // not labeled
-                //cout << "dfs " << r << "," << c << endl;
-                dfs(seg, r, c, label, img);
-                label++;
-            }
-        }
-    }
-
-
-    int n_circles = 0;
-    vector<std::vector<int> > viz(img.rows(), vector<int>(img.cols(), 0));
-    for (int r=0; r<img.rows(); ++r) {
-        for (int c=0; c<img.cols(); ++c) {
-            if (seg[r][c] != -1 && viz[r][c] == 0) {
-                n_circles += is_circle(r,c,viz,seg);
-            }
-        }
-    }
-
-    return n_circles;
 }
 
 
@@ -618,94 +479,7 @@ IGRAY copy_img (IGRAY& img) {
 }
 
 
-int find_optimal_n_circles (IGRAY img) {
-
-    int k = 1;
-    int n_circles = -1;
-
-    //auto cross = get_cross(2,2);
-
-    vector<vector<double> > cross = { 
-                                        {0,   0,   0,   1,   0,  0, 0},
-                                        {0,   0,   1,   1,   1,  0, 0},
-                                        {0,   1,   1,   1,   1,  1, 0},
-                                        {1,   1,   1,   1,   1,  1, 1},
-                                        {0,   1,   1,   1,   1,  1, 0},
-                                        {0,   0,   1,   1,   1,  0, 0},
-                                        {0,   0,   0,   1,   0,  0, 0}};
-
-
-    while (true) {
-        //cout << "iteration: " << k << endl;
-        //show_img(img);
-        img = img.erode(cross);
-        int cur_n_circles = get_n_circles (img);
-        //cout << "#circles found: " << cur_n_circles << endl;
-        if (cur_n_circles < n_circles) {
-            //k--;
-            break;
-        }
-        else {
-            n_circles = cur_n_circles;
-        }
-        k++;
-    }
-    return n_circles;
-}
-
-
-#if 0
-vector<pair<int,int> > watershed_maxima(IGRAY& img, int si, int sj, vector<vector<int> >& D) {
-    // img is thresholded: background = 0, non-background = 255
-
-
-    stack<pair<int,int> > S;
-    S.push({si,sj});
-
-    while(!S.empty()) {
-
-        auto e = S.top();
-        int i = e.first;
-        int j = e.second;
-        cout << i << "," << j << endl;
-
-        if(img.at(i,j) == 255) {
-
-            for(auto n : neighbors) {
-                int ni = i+n.first;
-                int nj = j+n.second;
-
-                if(in_bounds(ni,nj,img.rows(), img.cols())) {
-                    D[i][j] = 1 + min(D[ni][nj], D[i][j]);
-                    S.push({ni,nj});
-                }
-            }
-        }
-
-        S.pop();
-    }
-
-    IGRAY res(img.rows(), img.cols());
-    for (int r = 0; r < res.rows(); ++r) {
-        for (int c = 0; c < res.cols(); ++c) {
-            if (img.at(r,c) == 0) {
-                res.at(r,c,0);
-            }
-            else {
-                res.at(r,c,D[r][c] * 5);
-            }
-        }
-    }
-    show_img(res);
-
-
-    vector<pair<int,int> > maxi;
-    return maxi;
-}
-#endif
-
-
-int watershed_maxima_bfs(IGRAY& img, IGRAY& gray) {
+void watershed_maxima(IGRAY& img, IGRAY& gray) {
 
     vector<vector<int> > viz(img.rows(), vector<int>(img.cols(), 0));
     //queue<pair<pair<int,int>,double> > Q;
@@ -758,32 +532,6 @@ int watershed_maxima_bfs(IGRAY& img, IGRAY& gray) {
             }
         }
     }
-
-
-    //cout << Q.size() << endl;
-
-    #if 0
-    while(!Q.empty()) {
-        auto e = Q.front(); Q.pop();
-        int i = e.first.first;
-        int j = e.first.second;
-        double d = e.second;
-
-        for (auto n : neighbors) {
-            int ni = i+n.first;
-            int nj = j+n.second;
-            if (in_bounds(ni,nj,gray.rows(), gray.cols())) {
-                if (viz[ni][nj] == 0 && img.at(ni,nj) == 255) {
-                    double new_d = d+1; // sqrt((i-ni)*(i-ni) + (j-nj)*(j-nj));
-                    Q.push({{ni, nj}, new_d});
-                    gray.at(ni,nj, (3 * int(new_d)) % 256);
-                    viz[ni][nj] = 1;
-                }
-            }
-        }
-    }
-    #endif
-    return 0;
 }
 
 inline double l2(double x, double y, double q, double r) {
@@ -806,27 +554,17 @@ inline DD merge (DD a, DD b) {
 }
 
 
-
 void draw_maxi(IGRAY& gray, map<pair<double,double>, bool>& maxi) {
     for (auto r : maxi) {
         int i = r.first.first;
         int j = r.first.second;
-
         gray.at(i,j,255);
-        /*
 
-        if (gray.at(i,j) > 0) {
-            gray.at(i,j, 0);
-        }
-        else {
-            gray.at(i,j,255);
-        }*/
     }
-    //show_img(gray);
 }
 
 
-int find_optimal_circles(IGRAY& img, int ksize) {
+int find_n_circles(IGRAY& img, int ksize) {
 
     IGRAY gray (img.rows(), img.cols());
     for (int r = 0; r < img.rows(); ++r) {
@@ -835,8 +573,7 @@ int find_optimal_circles(IGRAY& img, int ksize) {
         }
     }
 
-    int r = watershed_maxima_bfs(img, gray);
-    //show_img(gray);
+    watershed_maxima(img, gray);
 
     int kh2 = ksize / 2;
     int kw2 = ksize / 2;
@@ -871,7 +608,6 @@ int find_optimal_circles(IGRAY& img, int ksize) {
             }
         }
     }
-    //show_img(dilated_gray);
 
     // local maximas are pixels in dilated img which are equal to ones in img
     for (int r = 0; r<gray.rows(); ++r) {
@@ -883,9 +619,6 @@ int find_optimal_circles(IGRAY& img, int ksize) {
             }
         }
     }
-    //cout << "#local maximas: " << maxi.size() << endl;
-    //draw_maxi(dilated_gray, maxi);
-
 
     vector<pair<double,double> > res;
     while (true) {
@@ -914,13 +647,6 @@ int find_optimal_circles(IGRAY& img, int ksize) {
         if(!merged)
             break;
     }
-    //show_img(gray);
-    //draw_maxi(gray, maxi);
-
-/*
-    for (auto r : maxi)
-        cout << r.first.first << ", " << r.first.second << endl;
-*/
     return maxi.size();
 }
 
@@ -937,36 +663,16 @@ int main(int argc, char** argv) {
     /**
      * Convert to grayscale
      */
-    //////cout << "Gray ..." << endl;
     IGRAY gray = rgb2gray(img);
-    //show_img(gray);
 
 
     /**
-     * Erosion
+     * Threshold
      */
-    //////cout << "Threshold ..." << endl;
-
     double bg = gray.at(10,10);
-    ////cout << "bg: " << bg << endl;
-
     IGRAY erosion_gray = gray.threshold(bg, 255, IGRAY::THRESHOLD_TYPE::THRESH_BINARY_BACKGROUND);
-    //show_img(erosion_gray);
 
-    cout << find_optimal_circles(erosion_gray, 15) << endl;
-
-    /*
-    ofstream fout("/Users/whoiscris/competitive_programming/HACKERRANK/ML/digital_image_analysis/count_the_circles/testcase5.out");
-    fout << erosion_gray.rows() << " " << erosion_gray.cols() << endl;
-    for (int r = 0; r < erosion_gray.rows(); ++r) {
-        for (int c = 0; c < erosion_gray.cols(); ++c) {
-            fout << erosion_gray.at(r,c) << " ";
-        }
-        fout << endl;
-    }
-    fout.close();
-    exit(0);
-    */
+    cout << find_n_circles(erosion_gray, 15) << endl;
 
     return 0;
 }
