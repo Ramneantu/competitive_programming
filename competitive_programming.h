3fd704b48973ce3033a64cbf41b8852a85e36b18
Cristian Plop <cristian.plop@tum.de>
2018-03-11 17:16:16 +0800

    add bellman-ford with improvement. Tested.

--- competitive_programming.h
+++ competitive_programming.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <cstdio>
 #include <vector>
@@ -7,9 +8,10 @@
 #include <queue>
 #include <cstring>
 #include <climits>
+#include <fstream>
+#include <algorithm>
 using namespace std;
 
-typedef long long LL;
 typedef int T;
 typedef vector<T> VT;
 typedef pair<T,T> TT;
@@ -28,35 +30,45 @@ typedef vector<VTT> AdjList;
  * Single Source Shortest Path
  * Works on Graphs with negative edges
  * If Graph contains a negative cycle then Bellman-Ford can detect it.
+ * Complexity: O(|V| * |E|)
+ * Improvement:
+ * For relaxation use only nodes that were improved.
+ * Complexity: O(|V| * |E| * log(|V|))¢
  */
-bool bellman(const AdjList& G, const int s, vector<int>& dist, vector<int>& parent) {
+bool bellman(const AdjList& G, const int s, vector<T>& dist, vector<int>& parent) {
 
 	int N = G.size();
 	dist.resize(N, INFINITY);
 	parent.resize(N, -1);
+	queue<int> Q;
+	vector<bool> in_queue(N, false);
+	vector<int> cnt_in_queue(N, 0);
 
 	// compute shortest paths from s to all vertices
 	dist[s] = 0;
-	for (int i=0; i<N-1; ++i) {
-		for (int u=0; u<G.size(); ++u) {
-			for (int j = 0; j<G[u].size(); ++j) {
-				int v = G[u][j].first;
-				int w = G[u][j].second;
-				if (dist[u] + w < dist[v]) {
-					dist[v] = dist[u] + w;
-					parent[v] = u;
-				}
-			}
-		}
-	}
-
-	// check for negative cycles
-	for (int u=0; u<G.size(); ++u) {
+	Q.push(s);
+	in_queue[s] = true;
+	cnt_in_queue[s] = 1;
+	while (!Q.empty()) {
+		int u = Q.front();
+		Q.pop();
+		in_queue[u] = false;
 		for (int j=0; j<G[u].size(); ++j) {
 			int v = G[u][j].first;
-			int w = G[u][j].second;
-			if (dist[u] + w < dist[v]) { // contains negative cycle
-				return true;
+			T w = G[u][j].second;
+			if (dist[u] != INFINITY && dist[u] + w < dist[v]) {
+				dist[v] = dist[u] + w;
+				if (!in_queue[v]) {
+					if (cnt_in_queue[v] >= N) {
+						return true;
+					}
+					else {
+						parent[v] = u;
+						Q.push(v);
+						in_queue[v] = true;
+						cnt_in_queue[v]++;
+					}
+				}
 			}
 		}
 	}
